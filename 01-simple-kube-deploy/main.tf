2de9e0ae2f3c36a63bfd0c432cbaca745c1a4c44
Chuck Cox <chuckc@us.ibm.com>
2020-11-17 21:26:00 -0600

    fix: my GH repo

--- 01-simple-kube-deploy/main.tf
+++ 01-simple-kube-deploy/main.tf
@@ -9,7 +9,7 @@ module "ibm-kubernetes-toolchain" {
   ibmcloud_api_key  = var.ibmcloud_api_key
   region            = "us-south"
   toolchain_name    = "terraform-toolchain-${random_string.random.result}"
-  application_repo  = "https://github.com/triceam/nodejs-express-app"
+  application_repo  = "https://github.com/ChuckCox/nodejs-express-app"
   resource_group    = var.resource_group
   cluster_name      = var.cluster_name
   cluster_namespace = var.cluster_namespace
