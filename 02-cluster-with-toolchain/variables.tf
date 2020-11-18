5d1eb56ed69524b7d9acee9e228f765623b80ec5
Chuck Cox <chuckc@us.ibm.com>
2020-11-17 23:09:24 -0600

    chore: added more descriptions

--- 02-cluster-with-toolchain/variables.tf
+++ 02-cluster-with-toolchain/variables.tf
@@ -1,6 +1,6 @@
 variable "ibmcloud_api_key" {
   type        = string
-  description = "The IAM API Key for IBM Cloud access"
+  description = "The IAM API Key for IBM Cloud access (https://cloud.ibm.com/iam/apikeys)"
 }
 
 variable "resource_group" {
@@ -10,34 +10,41 @@ variable "resource_group" {
 
 variable "cluster_name" {
   type        = string
-  description = "Name of Kubernetes Cluster to deploy into"
+  description = "Name of new Kubernetes cluster to create"
 }
 
 variable "resource_group_id" {
    default = ""
+   description = "The ID (not Account ID) from `ibmcloud resource group <NAME>`"
 }
 
 variable "machine_type" {
    default = "b2c.4x16"
+   description = "Name of machine type from `ibmcloud ks flavors --zone <ZONE>`"
 }
 variable "hardware" {
    default = "shared"
+   description = "The level of hardware isolation for your worker node. Use 'dedicated' to have available physical resources dedicated to you only, or 'shared' to allow physical resources to be shared with other IBM customers. For IBM Cloud Public accounts, the default value is shared. For IBM Cloud Dedicated accounts, dedicated is the only available option."
 }
 
 variable "datacenter" {
   default = "dal10"
+  description = "Zone from `ibmcloud ks zones --provider classic`"
 }
 
 variable "default_pool_size" {
   default = "1"
+  description = "How many worker nodes for the new Kubernetes cluster"
 }
 
 variable "private_vlan_num" {
   default = "1256"
+  description = "Number for private VLAN from `ibmcloud ks vlans --zone <ZONE>`"
 }
 
 variable "public_vlan_num" {
   default = "1410"
+  description = "Number for public VLAN from `ibmcloud ks vlans --zone <ZONE>`"
 }
 
 variable kube_version {
@@ -47,7 +54,7 @@ variable kube_version {
 
 variable "cluster_namespace" {
   type        = string
-  description = "Kubernetes namespace to deploy into"
+  description = "Kubernetes namespace to deploy into (`kubectl get ns`)"
 }
 
 variable "container_registry_namespace" {
