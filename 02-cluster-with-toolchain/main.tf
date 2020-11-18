fffb5383c32abc2eb9c8a9f42b9f56d1450e67a8
Chuck Cox <chuckc@us.ibm.com>
2020-11-17 22:30:57 -0600

    chore: update vlan vars

--- 02-cluster-with-toolchain/main.tf
+++ 02-cluster-with-toolchain/main.tf
@@ -9,7 +9,6 @@ data "ibm_resource_group" "group" {
 
 provider "ibm" {
   ibmcloud_api_key   = var.ibmcloud_api_key
-  version            = "~> 1.14"
 }
 
 resource "ibm_container_cluster" "cluster" {
@@ -19,8 +18,8 @@ resource "ibm_container_cluster" "cluster" {
   machine_type      = var.machine_type
   hardware          = var.hardware
   kube_version      = var.kube_version
-  public_vlan_id    = var.public_vlan_id
-  private_vlan_id   = var.private_vlan_id
+  public_vlan_id    = var.public_vlan_num
+  private_vlan_id   = var.private_vlan_num
   resource_group_id = data.ibm_resource_group.group.id
 }
 
