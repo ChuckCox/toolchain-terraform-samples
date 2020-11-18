1d61693f01712974b8e28210cad7d7b979ad20ed
Chuck Cox <chuckc@us.ibm.com>
2020-11-17 21:48:46 -0600

    chore: vlan variables

--- 02-cluster-with-toolchain/main.tf
+++ 02-cluster-with-toolchain/main.tf
@@ -18,8 +18,8 @@ resource "ibm_container_cluster" "cluster" {
   machine_type      = var.machine_type
   hardware          = var.hardware
   kube_version      = var.kube_version
-  public_vlan_id    = "1410"
-  private_vlan_id   = "1256"
+  public_vlan_id    = var.public_vlan_id
+  private_vlan_id   = var.private_vlan_id
   resource_group_id = data.ibm_resource_group.group.id
 }
 
