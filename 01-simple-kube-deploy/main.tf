008528788a731aa1192e52f923c82ddbffebedf0
Chuck Cox <chuckc@us.ibm.com>
2020-11-24 14:00:32 -0600

    test: new zip file

--- 01-simple-kube-deploy/main.tf
+++ 01-simple-kube-deploy/main.tf
@@ -5,11 +5,11 @@ resource "random_string" "random" {
 
 
 module "ibm-kubernetes-toolchain" {
-  source            = "github.com/triceam/ibm-kubernetes-toolchain-module"
+  source            = "github.com/IBM-Cloud/ibm-kubernetes-toolchain-module"
   ibmcloud_api_key  = var.ibmcloud_api_key
   region            = "us-south"
-  toolchain_name    = "terraform-toolchain-${random_string.random.result}"
-  application_repo  = "https://github.com/ChuckCox/nodejs-express-app"
+  toolchain_name    = "terraform-toolchain-${var.language}-${random_string.random.result}"
+  application_repo  = "https://github.com/IBM/${var.language}/archive/latest.zip"
   resource_group    = var.resource_group
   cluster_name      = var.cluster_name
   cluster_namespace = var.cluster_namespace
